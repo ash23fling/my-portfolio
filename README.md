bfbfb56cdd733697464567262bf93727bfacf6c8
ash23fling <mashutosh808@gmail.com>
2024-12-04 00:43:54 +0530

    ashutosh portfolio main

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# my-portfolio
+**Portfolio**
+<br/>
+<div align="center">
+  <img alt="Demo" src="public/mockup.png" />
+</div>
+<br/>
+<br/>
+
+Descibe myself and projects i have done for learning and fun purpose.
---
+++ src/components/About/About.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <Image
+            src="/aboutme.png"
+            alt="man-svgrepo"
+          />
+        </ScrollAnimation>
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+            Hello! My name is <strong>Ashutosh Mishra</strong>. I'm currently working as a Software Engineer in Rakuten Symphony for around 4 years. During my time here, I gained valuable experience through various entry-level positions, which greatly enhanced my work ethic, communication skills, and adaptability.
+            </ScrollAnimation>
+
+            <br /><br />
+            
+            <ScrollAnimation animateIn="fadeInLeft">
+            My journey into the world of technology began with a strong passion for problem-solving, which led me to pursue the Computer Applications program in my college. Despite its challenges, I remained determined and forged lasting friendships that supported me throughout this demanding program.
+            Throughout my studies, I had the opportunity to engage in fascinating projects. Notably, I contributed to the development of various projects which helped me gain knowledge and build confidence.
+            </ScrollAnimation>
+
+            <br /><br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              Working collaboratively within a team towards a shared objective has been an incredibly rewarding and unique experience for me. I am eager to continue exploring exciting projects in the future, with a particular interest in web development and open to learn new aspects.
+              <div className="tagline2">
+                I have become confident using the following technologies:
+              </div>
+            </ScrollAnimation>
+            
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt={stack.name} />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
